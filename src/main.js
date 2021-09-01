61b053dc9a6781923d9b04a68a225665ced844e8
lany365 <jdlchr@163.com>
2021-09-02 01:06:16 +0800

    OK

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载 element 组件库
+import ElementUI from 'element-ui'
 
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 加载全局样式文件
+import './styles/index.less'
+
+import request from './utils/request'
+
+const a = 123
+
+// 全局注册 element 组件库
+Vue.use(ElementUI)
+
+// 创建 Vue 根实例
+// 把 router 配置到根实例中
+// 通过 render 方法把 App 根组件渲染到 #app 入口节点
 Vue.config.productionTip = false
 
 new Vue({
